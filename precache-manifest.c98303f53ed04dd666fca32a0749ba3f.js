8bdf7343503ed5d5062c8610d7f82c78d017da2d
yonyalva <alexjalv01@yahoo.com>
2020-01-19 11:21:11 -0500

    First commit

---
+++ precache-manifest.c98303f53ed04dd666fca32a0749ba3f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7f0dbd61e7478672b164",
+    "url": "/castingagency/static/css/main.1a0ccaae.chunk.css"
+  },
+  {
+    "revision": "7f0dbd61e7478672b164",
+    "url": "/castingagency/static/js/main.7f0dbd61.chunk.js"
+  },
+  {
+    "revision": "20b3a0fc4ca9ae391600",
+    "url": "/castingagency/static/js/1.20b3a0fc.chunk.js"
+  },
+  {
+    "revision": "36ba821f3950eb9940b4",
+    "url": "/castingagency/static/js/runtime~main.36ba821f.js"
+  },
+  {
+    "revision": "9e02e1f27a904d8a85c07b2d5eee7805",
+    "url": "/castingagency/index.html"
+  }
+];
